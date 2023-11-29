e24e8cb7b3eb2c2b5b0ff57a4bb77a48cee51dee
aledx18 <alediiaz18@gmail.com>
2023-11-29 18:08:06 -0300

    aa

--- src/components/renderProfile.tsx
+++ src/components/renderProfile.tsx
@@ -1,5 +1,5 @@
 import { UserProfile } from '../app/interface'
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+// import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 
 interface profileProps {
   profile: UserProfile
@@ -9,10 +9,10 @@ export default function RenderProfile({ profile }: profileProps) {
   return (
     <div className='flex justify-between items-center p-2 font-bold'>
       <h1 className='text-2xl'>Good Morning {profile.display_name}</h1>
-      <Avatar>
+      {/* <Avatar>
         <AvatarImage src={profile.images[0].url} />
         <AvatarFallback>UE</AvatarFallback>
-      </Avatar>
+      </Avatar> */}
     </div>
   )
 }
